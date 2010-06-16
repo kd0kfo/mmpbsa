7029075202beae77beffb8682e09a5b2e4c9711e
David Coss <David.Coss@stjude.org>
2010-06-16 11:06:55 -0500

    Initial Commit. This commit simply marks the existence of the project. Files to be added.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: dcoss
+ *
+ * Created on June 16, 2010, 10:55 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    std::cout << "Add mmpbsa code" << std::endl;
+    return 0;
+}
+
