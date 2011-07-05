70cccacb08d13ded149e3f145efb16e781dc4995
David Coss (hostname: hal9000) <David.Coss@stjude.org>
2011-07-05 14:02:55 -0500

    Added molsurf from AmberTools (1.4) to repo (allowed per GPL)

--- src/molsurf/molsurf.h
+++ src/molsurf/molsurf.h
@@ -1,20 +1,4 @@
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-
-#ifdef __cplusplus
- extern "C" {
-#include <csetjmp>//:-(
-#endif
-
-
 #define ERROR (-1)
-#define REAL_T	double
-
-#define MAXAT 10000
-#define MAXRES 5000
-
 #define PI   3.14159265358979323846
 #define TWOPI 6.28318530717958647692
 #define Rad2Deg      (180.0/PI)
@@ -30,7 +14,7 @@
 #define MAX_FACE_CYCLES    4
 #define MAXTMP            20
 
-extern REAL_T __dx__, __dy__, __dz__;
+REAL_T __dx__, __dy__, __dz__;
 
 #define DOT(pi,pj) ( (pi)[0]*(pj)[0] + (pi)[1]*(pj)[1] + (pi)[2]*(pj)[2] )
 
@@ -39,7 +23,7 @@ extern REAL_T __dx__, __dy__, __dz__;
                ( (pi)[1] - (pj)[1] )* ( (pi)[1] - (pj)[1] ) + \
 	           ( (pi)[2] - (pj)[2] )* ( (pi)[2] - (pj)[2] )  ) )
 
-typedef REAL_T molsurfPOINT[3];
+typedef REAL_T POINT[3];
 
 #define NAME_SIZE       8
 
@@ -52,12 +36,13 @@ typedef struct res {
 } RES;
 
 typedef struct atom {
-	molsurfPOINT pos;
+	POINT pos;
 	REAL_T q, rad;
+#ifdef DEBUG
 	char anam[NAME_SIZE];
-	char rnam[NAME_SIZE];
 	int anum;
-	int rnum;
+	int res;
+#endif
 	int buried;	/* 1 = buried */
 	int neighbor_start, n_neighbors;
 	int upper_start, n_upper;   /* points to neighbor_torus struct */
@@ -70,7 +55,7 @@ typedef struct atom {
 	int n_cycles;       /*  cycles of edges associated with atom */
 	int cycle_start;    /*  points to start of cyclelist for the atom */
 	REAL_T area;    /* accessible surface area  associated with the atom */
-} molsurfATOM;
+} ATOM;
 
 typedef struct neighbor_torus {
 	int iatom;
@@ -83,7 +68,7 @@ typedef struct neighbor{
 
 /************* torus and probe data structures *********/
 typedef struct {
-        molsurfPOINT center, uv;       /* torus center and axis unit vector */
+        POINT center, uv;       /* torus center and axis unit vector */
         REAL_T rad;
         int a1, a2;
 	int concave_edges[MAXTOR_EDGE], n_concave_edges;
@@ -93,7 +78,7 @@ typedef struct {
 } TORUS;
 
 typedef struct probe {
-    molsurfPOINT pos; 
+    POINT pos; 
 	int a1, a2, a3;        /* atoms associated with probe */
 	int c1, c2, c3;		/* circles associated with probe */
 	REAL_T height;          /* height of probe above base plane */
@@ -103,7 +88,7 @@ typedef struct probe {
 /************* edge data structures ************/
 
 typedef struct vertex { 
-	molsurfPOINT pos; 
+	POINT pos; 
 	int iatom;      /* atom associated with vertex */
     int iprobe;   /* probe associated with vertex */
 	REAL_T beta;   /* concave triangle angle */
@@ -119,8 +104,8 @@ typedef struct circle {
     int torus;
 	int atom_or_probe_num;		/* concave = probe  convex = atom */
 	REAL_T rad;
-	molsurfPOINT center;
-	molsurfPOINT axis;
+	POINT center;
+	POINT axis;
 } CIRCLE;
 
 /************* face data structures ************/
@@ -218,7 +203,7 @@ typedef struct cusp_group {
    become real cusps, depending on the non_axial cusps that it intersects */
 
 typedef struct cusp_pair {
-	molsurfPOINT circle_center, circle_axis, vert1, vert2;
+	POINT circle_center, circle_axis, vert1, vert2;
 	REAL_T circle_rad;
 	int cycle1, cycle2, cycle3;
 	int cusp1, cusp2;		/*  cusps that intersect at new_cusp */
@@ -231,15 +216,123 @@ typedef struct extreme_vertex {
   int vert_index;		/* index in vertex[] array */
 } EXTREME_VERTEX;
 
-REAL_T
-molsurf (const REAL_T xs[], const REAL_T ys[], const REAL_T zs[],
-	 const REAL_T rads[], int nats, REAL_T probe_rad);
+#if 0
+/*  routine declarations   */
 
-#include <setjmp.h>
-
-extern jmp_buf jmpbuf;
-#ifdef __cplusplus
- }
- #endif
-
- 
+static void check_broken_faces ();
+static void add_edge ();
+static void add_free_edge ();
+static void add_probe ();
+static void add_saddle_face ();
+static void cross ();
+static void memory_usage ();
+static void vnorm ();
+static void write_verts ();
+static void atom_vertex_match ();
+static int getneighbors ();
+static int get_probes ();
+static int probe_pos ();
+static int no_bump ();
+static int t_buried ();
+static int bury_check ();
+static int is_buried ();
+static int get_torus ();
+static void torus_data ();
+static void sort_neighbors ();
+static int convex_circles ();
+static int concave_circles ();
+static void concave_edges ();
+static void addvert ();
+static void convex_edges ();
+static void check_data ();
+static void face_info ();
+static void check_convex_edges ();
+static void add_convex_edge_to_torus ();
+static void add_convex_edge_to_atom ();
+static void convex_faces ();
+static void sort_edges ();
+static void cycles ();
+static int new_edge ();
+static int next_edge ();
+static void dump_atom_edges ();
+static void draw_arc ();
+static void draw_circle ();
+static void draw_edges ();
+static int is_cycle_inside ();
+static REAL_T get_angle ();
+static REAL_T convex_area ();
+static REAL_T cycle_piece ();
+static void write_info ();
+static int concave_area ();
+static int saddle_area ();
+static int id_torus ();
+static void make_cones ();
+static int one_sided_torus ();
+static void add_2_verts ();
+static int get_low_torus_index ();
+static void make_2_cone_faces ();
+static void kill_saddle_face ();
+static void cone_init ();
+static void write_cone_info ();
+static void make_broken_faces ();
+static void add_concave_cycle ();
+static void broken_face_info ();
+static REAL_T get_cone_area ();
+static void axial_trim ();
+static int get_cycle_edge ();
+static void sort_faces ();
+static void add_circle ();
+static int cone_edge ();
+static void add_edges_2_cycle ();
+static void check_cycle ();
+static void concentric_axial_cusps ();
+static void reroute ();
+static void split_cycle ();
+static int next_cycle_edge ();
+static void non_axial_trim ();
+static int new_cusp_in_group ();
+static void cusp_intersect ();
+static int get_cycle_id ();
+static void get_probe_id ();
+static void add_new_cusp ();
+static int center_cycle ();
+static int number_of_cusps ();
+static int new_cusp ();
+static void make_circle ();
+static void trim_2_cusps ();
+static void unique_cycles ();
+static void trim_3_cusps ();
+static void split_3_cusps ();
+static void trim_4_cusps ();
+static void get_xvertex ();
+static REAL_T dist2 ();
+static void copy_vert ();
+static void get_3_xvertex ();
+static void copyvec ();
+static int get_cusps ();
+static int span_pairs ();
+static void dump_cycle ();
+static void add_2_cusps ();
+static void add_2cusp_verts ();
+static void split_old_cusps ();
+static void make_new_cusp ();
+static void get_faces ();
+static int is_new_face ();
+static void split_face ();
+static void get_starters ();
+static void copy_cycle ();
+static int cusp_match ();
+static int normal_match ();
+static void add_non_axial_cusp ();
+static void add_1_vert ();
+static void add_cusp_verts ();
+static void add_cusp_circle ();
+static void get_2_cycles ();
+static void add_1_cusp ();
+static void cusp_intersect_2 ();
+static int broken_concave_area ();
+static REAL_T conc_cycle_piece ();
+static REAL_T interior_angle ();
+static void allocate_memory ();
+static void free_memory ();
+#endif
